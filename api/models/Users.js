8e6e6786e3b436f310b22220af8dd50fdcac03ce
gitJank <justinjankiewicz@gmail.com>
2018-10-10 21:15:49 -0500

    added user model and controllers

---
+++ api/models/Users.js
@@ -0,0 +1,38 @@
+/**
+ * Users.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    email: {
+      type: 'string',
+      required: true,
+      unique: true,
+      isEmail: true,
+      maxLength: 200,
+      example: 'foo@bar.com'
+    },
+
+    password: {
+      type: 'string',
+      required: true,
+      description: 'Securely hashed representation of the user\'s login password.',
+      protect: true,
+      example: '2$28a8eabna301089103-13948134nad'
+    },
+
+    fullName: {
+      type: 'string',
+      required: true,
+      description: 'Full representation of the user\'s name',
+      maxLength: 120,
+      example: 'Tom Petty'
+    },
+  },
+};
+
